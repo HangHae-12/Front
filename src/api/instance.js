0219ab24a579bca45c0b9d957dcd7e3b60d3952d
baekjoowon <66767wndnjs@gamil.com>
2023-03-16 16:43:50 +0900

    [CHORE] 파일 디렉토리 수정

---
+++ src/api/instance.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: `${process.env.REACT_APP_TODOS}`,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+  },
+});
+
+instance.interceptors.request.use(
+  function (config) {
+    // 요청을 보내기 전 수행
+    console.log("인터셉트 요청 성공!");
+    const token = localStorage.getItem("access_token");
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  function (error) {
+    // 오류 요청을 보내기 전 수행
+    console.log("인터셉트 요청 오류!");
+    return Promise.reject(error);
+  }
+);
+
+instance.interceptors.response.use(
+  function (response) {
+    console.log("인터셉트 응답 받았어요!");
+    // 정상 응답
+    return response;
+  },
+
+  function (error) {
+    console.log("인터셉트 응답 못받았어요");
+    return Promise.reject(error);
+  }
+);
+
+export default instance;
