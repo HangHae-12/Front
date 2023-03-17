f1a35303fe61ccd47b152c9a570cdcb029f9dd1a
Cupcakes33 <rute930627@gmail.com>
2023-03-17 10:31:19 +0900

    [FIX] color theme 세미콜론 누락 수정

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,18 +1,18 @@
 const color = {
   black: "#000000",
-  while: "#ffffff",
-  gray_0: "808080",
-  gray_1: "7e7e7e",
-  gary_2: "d9d9d9",
-  gray_3: "ededed",
-  gray_4: "f0f0f0",
-  gray_5: "f3f3f3",
+  white: "#ffffff",
+  gray_0: "#808080",
+  gray_1: "#7e7e7e",
+  gary_2: "#d9d9d9",
+  gray_3: "#ededed",
+  gray_4: "#f0f0f0",
+  gray_5: "#f3f3f3",
   // gray color는 디자이너님께서 디자인 시스템 완성하시면 추가하겠습니다.
-  blue: "3aggff",
-  red: 'ff3636',
-  green: "31bb00",
-  orenge: "ff852d",
-  darkYellow: "d1bf20",
+  blue: "#3aggff",
+  red: "#ff3636",
+  green: "#31bb00",
+  orenge: "#ff852d",
+  darkYellow: "#d1bf20",
 };
 
 const defaultTheme = {
