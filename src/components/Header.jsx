ae99f1aacfbf538238f30f877b38461321139676
Cupcakes33 <rute930627@gmail.com>
2023-03-17 13:28:24 +0900

    [DOCS] NavBar -> Header 로 컴포넌트 이름 변경

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,32 +4,31 @@ import { DUMMY_IMG_SRC } from "../assets";
 
 const NavBar = () => {
   return (
-    <>
-      <StyledContainer>
-        <StyledLogoWrapper>
-          <img src={DUMMY_IMG_SRC} alt="서비스 로고" />
-          <h4>서비스 이름</h4>
-        </StyledLogoWrapper>
-        <StyledBrandWrapper>
-          {/* 버튼 컴포넌트로 리펙토링 할 부분 */}
-          <button className="brand">
-            <img src={DUMMY_IMG_SRC} alt="브랜드 로고" />
-            <p>스파르타 유치원</p>
-          </button>
-          <button className="bellBtn">
-            <AiOutlineBell />
-          </button>
-        </StyledBrandWrapper>
-      </StyledContainer>
-    </>
+    <StyledHeaderContainer>
+      <StyledLogoWrapper>
+        <img src={DUMMY_IMG_SRC} alt="서비스 로고" />
+        <h4>서비스 이름</h4>
+      </StyledLogoWrapper>
+      <StyledBrandWrapper>
+        {/* 버튼 컴포넌트로 리펙토링 할 부분 */}
+        <button className="brand">
+          <img src={DUMMY_IMG_SRC} alt="브랜드 로고" />
+          <p>스파르타 유치원</p>
+        </button>
+        <button className="bellBtn">
+          <AiOutlineBell />
+        </button>
+      </StyledBrandWrapper>
+    </StyledHeaderContainer>
   );
 };
 
 export default NavBar;
 
-const StyledContainer = styled.nav`
+const StyledHeaderContainer = styled.header`
   position: fixed;
   top: 0;
+  z-index: 10;
   display: flex;
   width: 100%;
   height: 80px;
@@ -38,6 +37,7 @@ const StyledContainer = styled.nav`
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
+  border-bottom: 1px solid ${({ theme }) => theme.color.gray_3};
 `;
 
 const StyledLogoWrapper = styled.div`
