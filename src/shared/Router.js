55e209e3024c42707b7ca581b238542955985c3a
baekjoowon <66767wndnjs@gamil.com>
2023-03-20 17:56:01 +0900

    [FEAT]관리자 레이아웃 추가

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Host from '../pages/Host/Host';
 import Preview from "../pages/Preview";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route path="/" element={< />} /> */}
+        <Route path="/" element={<Host />} />
         <Route path="/preview" element={<Preview />} />
       </Routes>
     </BrowserRouter>
