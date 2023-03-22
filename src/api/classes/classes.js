0dc6404b1f37012aced35bad88427af30a1eb6e3
Jae Yeon Hwang <>
2023-03-22 17:04:17 +0900

    [FEAT] 아이들, 갤러리 검색기능 추가/ 갤러리 페이지네이션 기능  추가

--- src/api/classes/classes.js
+++ src/api/classes/classes.js
@@ -1,36 +1,65 @@
 import instance from "../instance";
 
-
 export const getClassesPage = async (id) => {
-    try {
-      const response = await instance.get(`api/common/classes/${id}`);
-      return response;
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  try {
+    const response = await instance.get(`api/common/classes/${id}`);
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-  export const getClassesMember = async (id) => {
-    try {
-      const response = await instance.get(`api/common/classes/${id}/children`)
-      return response;
-    } catch (error) {
-      console.log(error);
-    }
-  };
+export const getClassesMember = async (id) => {
+  try {
+    const response = await instance.get(`api/common/classes/${id}/children`);
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-  export const setClassesTeacher = async (payload) => {
-    try {
-      const response = await instance.put(`api/managers/${payload.id}/teacher-profiles`,
+export const getClassesGallery = async (id, page, perPage) => {
+  try {
+    const response = await instance.get(`api/classes/${id}/gallery`, {
+      params: {
+        page,
+        perPage,
+      },
+    });
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getSearchGallery = async (keyword, page, perPage) => {
+  try {
+    const response = await instance.get("api/common/search/imagePosts", {
+      params: {
+        keyword,
+        page,
+        perPage,
+      },
+    });
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const setClassesTeacher = async (payload) => {
+  try {
+    const response = await instance.put(
+      `api/managers/${payload.id}/teacher-profiles`,
       {
         image: payload.image,
       },
       {
         headers: { "Content-Type": "multipart/form-data" },
       }
-      );
-      return response;
-    } catch (error) {
-      console.log(error);
-    }
-  };
+    );
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
--- src/pages/Classes/Gallery.jsx
+++ src/pages/Classes/Gallery.jsx
@@ -1,7 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import { useParams } from "react-router-dom";
+import { useQueryClient, useQuery, useMutation } from "@tanstack/react-query";
+import { getClassesGallery, getSearchGallery } from "../../api/classes/classes";
+import Pagination from "react-js-pagination";
 
 function Gallery() {
+  const queryClient = useQueryClient();
+  const { id } = useParams();
+  const [searchGallery, setSearchGallery] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+
+  // const { data } = useQuery(
+  //   ["classesGallery"],
+  //   () => getClassesGallery(id),
+  //   {
+  //     onSuccess: (data) => {
+  //       console.log(data);
+  //     },
+  //     onError: () => {
+  //       console.log("error");
+  //     },
+  //   }
+  // );
+
+  // const {data : searchResult} = useQuery(
+  //   ["getSearchGallery", searchGallery],
+  //   () => getSearchGallery(searchGallery),
+  //   {
+  //     enabled: !!searchGallery,
+  //   }
+  // )
+
+  const { data } = useQuery(
+    ["classesGallery", searchGallery, currentPage],
+    () => {
+      if (searchGallery) {
+        return getSearchGallery(searchGallery, currentPage, 15);
+      }
+      return getClassesGallery(id, currentPage, 15);
+    },
+    {
+      onSuccess: (data) => {
+        console.log(data);
+      },
+      onError: () => {
+        console.log("error");
+      },
+    }
+  );
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    setSearchGallery(e.target.value);
+    queryClient.invalidateQueries(["classesGallery", searchGallery]);
+  };
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
   const cards = [];
   for (let i = 0; i < 15; i++) {
     cards.push(
@@ -21,19 +79,41 @@ function Gallery() {
         <StyledGalleryHeader>
           <button>전체기간</button>
           <button style={{ marginLeft: "auto" }}>사진등록</button>
-          <input style={{ marginLeft: "10px" }}></input>
+          <input
+            style={{ marginLeft: "10px" }}
+            onChange={handleSearch}
+            placeholder="검색어를 입력하세요"
+          />
         </StyledGalleryHeader>
         <StyledGalleryContainer>
           {cards}
-          {/* <StyledGalleryCard>
-            <StyledGalleryImage src="https://blog.kakaocdn.net/dn/drkKUz/btrKzPmA6Xi/cLjppsVnQYYF2dggTuvCf0/img.png" />
-            <StyledTitleFont>제목</StyledTitleFont>
+          {/* {data?.map((item) => {
+            return (
+              <StyledGalleryCard key={item.data.imagePostId}>
+            <StyledGalleryImage src={item.data.imageUrlList} />
+            <StyledTitleFont>{item.data.title}</StyledTitleFont>
             <StyledFont>
-              <StyledDateFont>2023.03.21 14:30</StyledDateFont>
-              <StyledDateFont>황재연</StyledDateFont>
+              <StyledDateFont>{item.data.createdAt}</StyledDateFont>
+              <StyledDateFont>{item.data.name}</StyledDateFont>
             </StyledFont>
-          </StyledGalleryCard> */}
+          </StyledGalleryCard>
+            );
+          })} */}
         </StyledGalleryContainer>
+        <PaginationContainer>
+          <Pagination
+            activePage={currentPage}
+            itemsCountPerPage={15}
+            // totalItemsCount={data?.totalItemsCount}
+            totalItemsCount={16}
+            pageRangeDisplayed={5}
+            onChange={handlePageChange}
+            innerClass="pagination"
+            itemClass="page-item"
+            linkClass="page-link"
+            activeClass="active"
+          />
+        </PaginationContainer>
       </StyledGalleryWrapper>
     </>
   );
@@ -117,4 +197,41 @@ const StyledDateFont = styled.div`
 const StyledFont = styled.div`
   display: flex;
   justify-content: space-between;
+`;
+
+const PaginationContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 16px;
+
+  .pagination {
+    display: flex;
+    list-style-type: none;
+    padding-inline-start: 0;
+  }
+
+  .page-item {
+    margin: 0 4px;
+  }
+
+  .page-link {
+    display: inline-block;
+    padding: 8px 12px;
+    border-radius: 4px;
+    color: #000000;
+    text-decoration: none;
+    background-color: #f5f5f5;
+    transition: background-color 0.2s;
+    cursor: pointer;
+  }
+
+  .page-link:hover {
+    background-color: #e0e0e0;
+  }
+
+  .active .page-link {
+    background-color: #2f80ed;
+    color: #ffffff;
+  }
 `;
