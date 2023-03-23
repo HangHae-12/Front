ddb92cd408c20faa5be882025b9fec456b4faafc
Cupcakes33 <rute930627@gmail.com>
2023-03-23 16:29:23 +0900

    [FIX] color Helpers 를 theme color 로 롤백

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,30 +1,32 @@
-import { COLOR } from "../helpers/color";
 import { transColor } from "../utils/transColor";
 
-const color = {
-  primary: COLOR.primary,
-  primary_l30: transColor.lighten(COLOR.primary, 0.3),
+const grayScale = {
+  800: "#2c2c2c",
+  700: "#383838",
+  600: "#424242",
+  500: "#757575",
+  400: "#9e9e9e",
+  300: "#bdbdbd",
+  200: "#dadada",
+  100: "#e9e9e9",
+  50: "#f9f9f9",
+};
 
-  black: COLOR.grayScale[900],
-  gray_800: COLOR.grayScale[800],
-  gray_700: COLOR.grayScale[700],
-  gray_600: COLOR.grayScale[600],
-  gray_500: COLOR.grayScale[500],
-  gray_400: COLOR.grayScale[400],
-  gray_300: COLOR.grayScale[300],
-  gray_200: COLOR.grayScale[200],
-  gray_100: COLOR.grayScale[100],
-  gray_50: COLOR.grayScale[50],
-  white: COLOR.grayScale[0],
-  blue: COLOR.blue,
-  red: COLOR.red,
-  orange: COLOR.orange,
-  yellow: COLOR.yellow,
-  green: transColor.lighten(COLOR.green, 0.4),
+const color = {
+  black: "#212121",
+  white: "#ffffff",
+  primary: "#56bea4",
+  primary_l30: transColor.lighten("#56bea4", 0.3),
+  blue: "#567bff",
+  red: "#f35451",
+  orange: "#ff852d",
+  yellow: "#ffd600",
+  green: transColor.lighten("#00c814", 0.4),
+  grayScale: grayScale,
 };
 
 const defaultTheme = {
-  color,
+  color: color,
 };
 
 export default defaultTheme;
