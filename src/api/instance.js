007e7bba0fc21bebb891b2495c08f6109c946f8f
Cupcakes33 <rute930627@gmail.com>
2023-03-29 23:12:59 +0900

    [FEAT] signup hook form 로직 구현

--- src/api/instance.js
+++ src/api/instance.js
@@ -8,7 +8,6 @@ const instance = axios.create({
 
 instance.interceptors.request.use(
   function (config) {
-    console.log("인터셉트 요청 성공!");
     const token = tokenCookie.get();
     if (token) {
       config.headers["Authorization"] = `Bearer ${token}`;
@@ -16,20 +15,16 @@ instance.interceptors.request.use(
     return config;
   },
   function (error) {
-    console.log("인터셉트 요청 오류!");
     return Promise.reject(error);
   }
 );
 
 instance.interceptors.response.use(
   function (response) {
-    console.log("인터셉트 응답 받았어요!");
     return response;
   },
 
   function (error) {
-    console.log("인터셉트 응답 못받았어요");
-
     if (error.response && error.response.status === 401) {
       tokenCookie.remove();
       alert("인증이 만료되었습니다. 다시 로그인 해주세요.");
--- src/pages/Login/SignupParent.jsx
+++ src/pages/Login/SignupParent.jsx
@@ -1,4 +1,102 @@
+import { useForm } from "react-hook-form";
+import { useMutation } from "@tanstack/react-query";
+import styled from "styled-components";
+import textVariants from "../../styles/variants/textVariants";
+
 const SignupParent = () => {
-  return (<>부모님</>)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const mutation = useMutation(async (data) => {
+    console.log(data);
+  });
+
+  const onSubmit = (data) => {
+    mutation.mutate(data);
+  };
+
+  return (
+    <StyledSignup.Container>
+      <StyledSignup.Form onSubmit={handleSubmit(onSubmit)}>
+        <StyledSignup.Label htmlFor="name">이름 *</StyledSignup.Label>
+        <StyledSignup.Input
+          type="text"
+          {...register('name', { required: '이름을 입력해주세요.' })}
+          id="name"
+        />
+        {errors.name && (
+          <StyledSignup.ErrorMessage>
+            {errors.name.message}
+          </StyledSignup.ErrorMessage>
+        )}
+
+        <StyledSignup.Label htmlFor="phoneNumber">휴대폰 번호 *</StyledSignup.Label>
+        <StyledSignup.Input
+          type="text"
+          {...register('phoneNumber', {
+            required: '휴대폰 번호를 입력해주세요',
+          })}
+          id="phoneNumber"
+        />
+        {errors.phoneNumber && (
+          <StyledSignup.ErrorMessage>
+            {errors.phoneNumber.message}
+          </StyledSignup.ErrorMessage>
+        )}
+
+        <StyledSignup.Label htmlFor="profileImage">프로필 사진</StyledSignup.Label>
+        <StyledSignup.Input
+          type="file"
+          {...register('profileImage')}
+          id="profileImage"
+        />
+
+        <StyledSignup.Label htmlFor="relationship">관계</StyledSignup.Label>
+        <StyledSignup.Input
+          type="text"
+          {...register('relationship')}
+          id="relationship"
+        />
+
+        <StyledSignup.Label htmlFor="emergencyPhoneNumber">비상연락망</StyledSignup.Label>
+        <StyledSignup.Input
+          type="text"
+          {...register('emergencyPhoneNumber')}
+          id="emergencyPhoneNumber"
+        />
+
+        <StyledSignup.Button type="submit">Submit</StyledSignup.Button>
+      </StyledSignup.Form>
+    </StyledSignup.Container>
+  );
 }
-export default SignupParent
+
+export default SignupParent;
+
+const StyledSignup = {
+  Container: styled.div`
+    display: flex;
+    width: 300px;
+    flex-direction: column;
+  `,
+
+  Form: styled.form`
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+  `,
+
+  Label: styled.label`
+    ${textVariants.Body2_Bold}
+  `,
+
+  Input: styled.input``,
+
+  Button: styled.button``,
+
+  ErrorMessage: styled.span`
+    color: ${({ theme }) => theme.color.red};
+  `,
+};
