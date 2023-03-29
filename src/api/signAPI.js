d8f5b7accd9aee964d44d759ffe466e67fae6597
Cupcakes33 <rute930627@gmail.com>
2023-03-29 22:42:07 +0900

    [FEAT] signup 관련 api 추가

--- src/api/signAPI.js
+++ src/api/signAPI.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import instance from "./instance";
 
 export const SignAPI = {
   kakaoAuth: async (code, cancelToken) => {
@@ -15,7 +16,31 @@ export const SignAPI = {
       // return data;
       return response;
     } catch (error) {
-      console.error("Error", error);
+      console.error(error);
+    }
+  },
+  signParent: async (info) => {
+    try {
+      const response = await axios.put(
+        "https://my-frist-server.shop/signup/parent/info",
+        // 주소가 확정되면 instance로 수정할 것
+        info
+      );
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  },
+  signTeacher: async (info) => {
+    try {
+      const response = await axios.put(
+        "https://my-frist-server.shop/signup/teacher/info",
+        // 주소가 확정되면 instance로 수정할 것
+        info
+      );
+      console.log(response);
+    } catch (error) {
+      console.error(error);
     }
   },
 };
