8893b360a5d6534417d3989e39ad222878679c86
Cupcakes33 <rute930627@gmail.com>
2023-03-29 15:17:18 +0900

    [FIX] sighAPI 수정. 소셜 로그인 토큰 발급을 서버에서 처리.

--- src/api/signAPI.js
+++ src/api/signAPI.js
@@ -1,32 +1,7 @@
 import axios from "axios";
-import { ENV } from "../helpers/envs";
 
 export const SignAPI = {
-  requestKakaoToken: async (code) => {
-    try {
-      const response = await axios.post(
-        "https://kauth.kakao.com/oauth/token",
-        null,
-        {
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-          },
-          params: {
-            grant_type: "authorization_code",
-            client_id: ENV.kakao_key,
-            redirect_uri: ENV.kakao_redirect,
-            code: code,
-          },
-        }
-      );
-      const data = response.data;
-      return data;
-    } catch (error) {
-      console.error("Error", error);
-    }
-  },
-
-  auth: async (code, cancelToken) => {
+  kakaoAuth: async (code, cancelToken) => {
     try {
       const response = await axios.get(
         `https://my-frist-server.shop/oauth/kakao/callback?code=${code}`,
@@ -36,8 +11,9 @@ export const SignAPI = {
           cancelToken: cancelToken,
         }
       );
-      const data = response.data;
-      return data;
+      // const data = response.data;
+      // return data;
+      return response;
     } catch (error) {
       console.error("Error", error);
     }
