a046d4f9b2d2b21cd4882e0ecbac896c43284004
Cupcakes33 <rute930627@gmail.com>
2023-03-29 23:18:33 +0900

    [STYLE] StyledSignup 코드분할

---
+++ src/pages/Login/signup/styled.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+import textVariants from "../../../styles/variants/textVariants";
+
+const StyledSignup = {
+  Container: styled.div`
+    display: flex;
+    width: 300px;
+    flex-direction: column;
+  `,
+
+  Form: styled.form`
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+  `,
+
+  Label: styled.label`
+    ${textVariants.Body2_Bold}
+  `,
+
+  Input: styled.input``,
+
+  Button: styled.button``,
+
+  ErrorMessage: styled.span`
+    color: ${({ theme }) => theme.color.red};
+  `,
+};
+
+export default StyledSignup;
