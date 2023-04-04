bd369feea98d833e4c7fd8ced2bf7f642e5425e6
Cupcakes33 <rute930627@gmail.com>
2023-04-04 15:31:11 +0900

    [FEAT] 아이관리 페이지 원생 프로필 컴포넌트 완성

--- src/pages/ChildManage/index.js
+++ src/pages/ChildManage/index.js
@@ -1,13 +1,44 @@
+import { useState } from "react";
 import styled from "styled-components";
+import { DUMMY_PROFILE_IMG_SRC } from "../../assets";
 import textVariants from "../../styles/variants/textVariants";
 
 const ChildManage = () => {
+  const [isFixMode, setIsFixMode] = useState(true);
   return (
     <StyledChildManage.Container>
       <StyledChildManage.Header>아이 관리</StyledChildManage.Header>
       <StyledChildManage.Section>
         <StyledCard>
           <StyledChildManage.Title>원생 프로필</StyledChildManage.Title>
+          <StyledChildManage.ProfileBox>
+            <img
+              src={DUMMY_PROFILE_IMG_SRC}
+              alt="profile"
+              style={{ width: "120px" }}
+            />
+            <StyledChildManage.InfoBox>
+              <li>
+                <StyledChildManage.SubTitle>이름</StyledChildManage.SubTitle>
+                <span>김민재</span>
+              </li>
+              <li>
+                <StyledChildManage.SubTitle>성별</StyledChildManage.SubTitle>
+                <span>남자</span>
+              </li>
+              <li>
+                <StyledChildManage.SubTitle>
+                  생년월일
+                </StyledChildManage.SubTitle>
+                <span>2015.12.07</span>
+              </li>
+            </StyledChildManage.InfoBox>
+          </StyledChildManage.ProfileBox>
+          <StyledChildManage.SubTitle>특이사항</StyledChildManage.SubTitle>
+          <StyledChildManage.SignificantArea
+            readOnly={isFixMode}
+            defaultValue="우리 아이는 너무 귀엽습니다."
+          />
         </StyledCard>
         <StyledCard></StyledCard>
       </StyledChildManage.Section>
@@ -18,6 +49,7 @@ const ChildManage = () => {
 export default ChildManage;
 
 const StyledCard = styled.div`
+  width: 100%;
   border-radius: 12px;
   padding: 40px;
   border: 1px solid ${({ theme }) => theme.color.grayScale[200]};
@@ -28,7 +60,7 @@ const StyledChildManage = {
     width: 100%;
     height: 100%;
     min-height: 100vh;
-    padding: 70px 150px;
+    padding: 70px 70px 0 150px;
   `,
   Header: styled.h2`
     ${textVariants.H2_Bold}
@@ -39,14 +71,67 @@ const StyledChildManage = {
   Section: styled.section`
     display: flex;
     flex-direction: row;
+    justify-content: space-between;
     width: 100%;
     height: 580px;
     gap: 24px;
-    border: 1px solid black;
   `,
 
   Title: styled.h3`
     ${textVariants.Body1_Bold}
     color: ${({ theme }) => theme.color.grayScale[700]};
+    opacity: 0.8;
+  `,
+
+  SubTitle: styled.p`
+    ${textVariants.Body2_SemiBold}
+    color: ${({ theme }) => theme.color.grayScale[500]};
+    opacity: 0.8;
+  `,
+
+  ProfileBox: styled.div`
+    width: 100%;
+    height: 120px;
+    /* height: min-content; */
+    margin: 24px 0px;
+    display: flex;
+    flex-direction: row;
+    gap: 90px;
+  `,
+
+  InfoBox: styled.ul`
+    display: flex;
+    width: 100%;
+    flex-direction: column;
+    justify-content: space-evenly;
+
+    li {
+      display: flex;
+      width: 100%;
+      justify-content: space-between;
+      align-items: center;
+    }
+
+    p,
+    span {
+      padding: 10px;
+    }
+
+    span {
+      ${textVariants.Body1_SemiBold}
+      color: ${({ theme }) => theme.color.grayScale[500]};
+      background-color: ${({ theme }) => theme.color.grayScale[50]};
+    }
+  `,
+
+  SignificantArea: styled.textarea`
+    width: 100%;
+    height: 200px;
+    background-color: ${({ theme }) => theme.color.grayScale[50]};
+    margin-top: 7px;
+    border: none;
+    resize: none;
+    outline: none;
+    padding: 10px;
   `,
 };
