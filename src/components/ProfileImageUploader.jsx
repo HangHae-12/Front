c3fd043b5dad970f76fa7c7791dcf84f83125802
Cupcakes33 <rute930627@gmail.com>
2023-04-05 21:44:02 +0900

    [REFECTOR] 프로필 이미지 업로더 리펙토링. 커스텀 훅으로 상태를 관리

--- src/components/ProfileImageUploader.jsx
+++ src/components/ProfileImageUploader.jsx
@@ -1,56 +1,38 @@
-import { useState, forwardRef, useImperativeHandle } from "react";
 import styled from "styled-components";
+import { DUMMY_URL } from "../helpers/dummyUrl";
+import { useProfileImageUploader } from "../hooks/useProfileImageUploader";
 
-const ProfileImageUploader = forwardRef((props, ref) => {
-  const { prev } = props;
-  const [isCancelled, setIsCancelled] = useState(false);
-  const [selectedFile, setSelectedFile] = useState(null);
-  const [previewImage, setPreviewImage] = useState(null);
-
-  useImperativeHandle(ref, () => ({
-    getProfileImageData: () => ({
-      isCancelled,
-      selectedFile,
-      previewImage,
-    }),
-  }));
+const ProfileImageUploader = ({ prev }) => {
+  const {
+    inputRef,
+    previewImage,
+    handleProfileImageChange,
+    handleProfileImageCancel,
+  } = useProfileImageUploader(prev);
 
   const handleImageChange = (e) => {
     const file = e.target.files[0];
-    console.log(file);
-    setIsCancelled(false);
 
     if (file) {
       const reader = new FileReader();
       reader.onloadend = () => {
-        setPreviewImage(reader.result);
+        handleProfileImageChange(file, reader.result);
       };
       reader.readAsDataURL(file);
-      setSelectedFile(file);
-    } else {
-      setPreviewImage(null);
     }
-    e.target.value = null;
   };
 
-  const handleThumbnailImage = () => ref.current.click();
-
-  const handleProfileImageCancel = () => {
-    setIsCancelled(true);
-    setSelectedFile(null);
-    setPreviewImage(prev);
-  };
+  const handleThumbnailImage = () => inputRef.current.click();
 
   return (
     <>
       <StyledProfileImageUploader.Input
         type="file"
-        {...props}
         onChange={handleImageChange}
-        ref={ref}
+        ref={inputRef}
       />
       <StyledProfileImageUploader.Thumbnail
-        src={previewImage ?? prev}
+        src={previewImage ?? prev ?? DUMMY_URL.not_profile_img}
         alt="Profile thumbnail"
         onClick={handleThumbnailImage}
       />
@@ -59,10 +41,9 @@ const ProfileImageUploader = forwardRef((props, ref) => {
       </button>
     </>
   );
-});
+};
 
 export default ProfileImageUploader;
-
 
 const StyledProfileImageUploader = {
   Input: styled.input`
