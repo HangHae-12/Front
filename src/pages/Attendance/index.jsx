6339aec42083c7a90228c4efbe6096c7bf36af7b
baekjoowon <66767wndnjs@gamil.com>
2023-04-05 17:00:48 +0900

    [FEAT] 데이터 피커 커스텀 추가

--- src/pages/Attendance/index.jsx
+++ src/pages/Attendance/index.jsx
@@ -1,75 +1,36 @@
-import React, { useState } from "react";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
-import * as XLSX from "xlsx";
-
-const Attendance = () => {
-  const [selectedDate, setSelectedDate] = useState(new Date());
-  const [attendanceData, setAttendanceData] = useState([
-    { No: 1, name: "백주원", status: "출석", entertime: "오전 9:00", exittime: "오후 4:30", etc: "" },
-    { No: 2, name: "김주원", status: "인정결석", entertime: "오전 9:00", exittime: "오후 7:30", etc: "코로나" },
-
-  ]);
-
-  const handleDateChange = (date) => {
-    setSelectedDate(date);
-  };
-
-  const exportToExcel = () => {
-    const ws = XLSX.utils.json_to_sheet(attendanceData, {
-      header: ["No", "name", "status", "entertime", "exittime", "etc"],
-    });
-    const wb = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(wb, ws, "월별 출석부");
-    const excelBuffer = XLSX.write(wb, { bookType: "xlsx", type: "array" });
-    const dataBlob = new Blob([excelBuffer], {
-      type:
-        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8",
-    });
-    XLSX.writeFile(wb, `출석부.xlsx`);
-  };
-
-  const filteredAttendanceData = attendanceData.filter(
-    (data) =>
-      new Date(data.date).toLocaleDateString() ===
-      selectedDate.toLocaleDateString()
-  );
+import styled from "styled-components";
+import SideBar from "../../components/SideBar";
+import Table from "./Table"
+const Host = () => {
 
   return (
-    <div>
-      <h1>일별 출석부</h1>
-      <DatePicker
-        selected={selectedDate}
-        onChange={handleDateChange}
-        dateFormat="yyyy-MM-dd"
-      />
-      <table>
-        <thead>
-          <tr>
-            <th>No.</th>
-            <th>이름</th>
-            <th>출결상태</th>
-            <th>등원시간</th>
-            <th>하원시간</th>
-            <th>결석사유</th>
-          </tr>
-        </thead>
-        <tbody>
-          {filteredAttendanceData.map((row, rowIndex) => (
-            <tr key={rowIndex}>
-              <td>{row.No}</td>
-              <td>{row.name}</td>
-              <td>{row.status}</td>
-              <td>{row.entertime}</td>
-              <td>{row.exittime}</td>
-              <td>{row.etc}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <button onClick={exportToExcel}>월별 출석부 엑셀로 내보내기</button>
-    </div>
+    <StyledContainer>
+      <StyledLeftContainer>
+        <SideBar />
+      </StyledLeftContainer>
+      <StyledRightContainer>
+        <Table />
+      </StyledRightContainer >
+    </StyledContainer >
   );
 };
 
-export default Attendance;
+export default Host;
+
+const StyledContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const StyledLeftContainer = styled.div`
+  width: 200px;
+  height: 100vh;
+`;
+
+const StyledRightContainer = styled.div`
+  flex-grow: 1;
+  margin-top: 60px;
+  margin-bottom: 94px;
+  margin-left: 150px;
+  margin-right: 150px;
+`;
