199e955c570a93fe53f74539c8babe430118c893
Cupcakes33 <rute930627@gmail.com>
2023-04-07 16:57:31 +0900

    [FEAT] 월~금 달력 컴포넌트 추가

--- src/pages/ChildManage/AttendanceCalendar.jsx
+++ src/pages/ChildManage/AttendanceCalendar.jsx
@@ -1,22 +1,20 @@
 import React, { useState } from "react";
 import moment from "moment";
 import styled from "styled-components";
+import textVariants from "../../styles/variants/textVariants";
+
+const DATA = [
+  {
+    child_id: 0,
+    date: "2023-04-03",
+    enter_time: "07:30",
+    exit_time: "16:30",
+    status: "출석",
+  },
+];
 
 const AttendanceCalendar = () => {
   const [currentMonth, setCurrentMonth] = useState(moment());
-  const startOfMonth = currentMonth.clone().startOf("month");
-  const endOfMonth = currentMonth.clone().endOf("month");
-  const weekdays = ["월", "화", "수", "목", "금"];
-
-  let days = [];
-  let currentDay = startOfMonth;
-
-  while (currentDay <= endOfMonth) {
-    if (currentDay.isoWeekday() <= 5) {
-      days.push(currentDay.clone());
-    }
-    currentDay = currentDay.add(1, "day");
-  }
 
   const goToPrevMonth = () => {
     setCurrentMonth(currentMonth.clone().subtract(1, "month"));
@@ -26,54 +24,103 @@ const AttendanceCalendar = () => {
     setCurrentMonth(currentMonth.clone().add(1, "month"));
   };
 
+  const renderDays = () => {
+    const days = [];
+    const startOfMonth = currentMonth.clone().startOf("month");
+    const endOfMonth = currentMonth.clone().endOf("month");
+    let currentDay = startOfMonth.clone().startOf("isoWeek");
+
+    while (currentDay.isSameOrBefore(endOfMonth)) {
+      if (currentDay.isoWeekday() <= 5) {
+        days.push(
+          <Day key={currentDay.format("YYYY-MM-DD")}>
+            {console.log(
+              currentDay.format("YYYY-MM-DD") >
+                currentMonth.format("YYYY-MM-DD")
+            )}
+            {currentDay.month() === currentMonth.month() &&
+            currentDay.format("YYYY-MM-DD") <=
+              currentMonth.format("YYYY-MM-DD") ? (
+              <>
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    marginBottom: "37px",
+                  }}
+                >
+                  <Date>{currentDay.date()}</Date>
+                  <AttendanceLabel isAttendance={true} />
+                </div>
+                <div>
+                  <div
+                    style={{
+                      display: "flex",
+                      flexDirection: "row",
+                      justifyContent: "space-between",
+                    }}
+                  >
+                    <div>등원</div>
+                    <div>07:30분</div>
+                  </div>
+                  <div
+                    style={{
+                      display: "flex",
+                      flexDirection: "row",
+                      justifyContent: "space-between",
+                    }}
+                  >
+                    <div>하원</div>
+                    <div>16:30분</div>
+                  </div>
+                </div>
+              </>
+            ) : null}
+          </Day>
+        );
+      }
+      currentDay = currentDay.add(1, "day");
+    }
+
+    return days;
+  };
   return (
     <>
-      <CalendarHeader>
-        <div>
-          <h2>출석 기록</h2>
-          <div className="toolbar-controls">
-            <button onClick={() => goToPrevMonth()}>&lt;</button>
-            <span>
-              {currentMonth.format("YYYY")}년 {currentMonth.format("M")}월
-            </span>
-            <button onClick={() => goToNextMonth()}>&gt;</button>
+      <CalendarWrapper>
+        <CalendarHeader>
+          <div>
+            <h2>출석 기록</h2>
+            <div className="toolbar-controls">
+              <button onClick={goToPrevMonth}>&lt;</button>
+              <span>
+                {currentMonth.format("YYYY")}년 {currentMonth.format("M")}월
+              </span>
+              <button onClick={goToNextMonth}>&gt;</button>
+            </div>
           </div>
-        </div>
-        <div>
-          <span>출석 {0}</span>
-          <span>결석 {0}</span>
-        </div>
-      </CalendarHeader>
-      <StyledAttendanceCalendar>
-        {weekdays.map((weekday) => (
-          <div key={weekday}>{weekday}</div>
-        ))}
-        {days.map((day) => (
-          <div key={day.format("YYYY-MM-DD")}>{day.format("D")}</div>
-        ))}
-      </StyledAttendanceCalendar>
+          <div>
+            <span>출석 {0}</span>
+            <span>결석 {0}</span>
+          </div>
+        </CalendarHeader>
+        <WeekdayWrapper>
+          {["월", "화", "수", "목", "금"].map((weekday) => (
+            <Weekday key={weekday}>{weekday}</Weekday>
+          ))}
+        </WeekdayWrapper>
+        <CalendarBody>{renderDays()}</CalendarBody>
+      </CalendarWrapper>
     </>
   );
 };
 
 export default AttendanceCalendar;
 
-const StyledAttendanceCalendar = styled.div`
-  display: grid;
-  grid-template-columns: repeat(5, 1fr);
-  gap: 1rem;
-`;
-
-const CalendarHeader = styled.div`
+const CalendarWrapper = styled.div`
   display: flex;
-  justify-content: space-between;
+  flex-direction: column;
   align-items: center;
-  padding: 1rem;
-
-  h2 {
-    margin: 0;
-    margin-bottom: 0.5rem;
-  }
 
   .toolbar-controls {
     display: flex;
@@ -87,8 +134,77 @@ const CalendarHeader = styled.div`
       padding: 0 0.5rem;
     }
   }
+`;
 
-  span {
-    margin: 0 1rem;
+const CalendarBody = styled.div`
+  display: grid;
+  width: 100%;
+  max-width: 1000px;
+  grid-template-columns: repeat(5, 1fr);
+`;
+
+const Day = styled.span`
+  position: relative;
+  width: 200px;
+  height: 160px;
+  padding: 17px 17px;
+  border: 1px solid ${({ theme }) => theme.color.grayScale[200]};
+`;
+
+const Date = styled.div`
+  position: relative;
+  ${textVariants.H3_SemiBold}
+  color: ${({ theme }) => theme.color.grayScale[500]}
+`;
+
+const CalendarHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  padding: 1rem;
+`;
+
+const WeekdayWrapper = styled.div`
+  ${textVariants.Body1_Bold}
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  width: 100%;
+  color: ${({ theme }) => theme.color.grayScale[500]};
+  background: ${({ theme }) => theme.color.grayScale[50]};
+`;
+
+const Weekday = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 200px;
+  height: 40px;
+  border: 1px solid ${({ theme }) => theme.color.grayScale[200]};
+  border-bottom: none;
+`;
+
+const AttendanceLabel = styled.div`
+  position: relative;
+
+  width: 42px;
+  height: 42px;
+  border-radius: 50%;
+  transform: rotate(-30deg);
+  border: 2px solid
+    ${({ theme, isAttendance }) =>
+      isAttendance ? theme.color.primary : theme.color.perple};
+
+  &::before {
+    ${textVariants.Body2_SemiBold}
+    content: ${({ isAttendance }) => (isAttendance ? `"출석"` : `"결석"`)};
+    display: block;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    color: ${({ theme, isAttendance }) =>
+      isAttendance ? theme.color.primary : theme.color.perple};
+    white-space: nowrap;
   }
 `;
