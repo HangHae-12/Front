c054b9ec36011582faa93182e1ed1a5f15bd6b9a
Cupcakes33 <rute930627@gmail.com>
2023-04-10 11:43:28 +0900

    [DOCS] big logo 추가

--- src/helpers/assets.js
+++ src/helpers/assets.js
@@ -1,8 +1,10 @@
 import logo from "../assets/kindergrew_logo.png";
+import big_logo from "../assets/kindergrew_big_logo.png";
 import kakao_login_btn from "../assets/kakao_login_medium_wide.png";
 
 const ASSETS = {
   logo: logo,
+  big_logo: big_logo,
   kakao_login_btn: kakao_login_btn,
 };
 
