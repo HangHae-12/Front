670c23e3c2fcc430302004b73b1555cbcc01ba59
Cupcakes33 <rute930627@gmail.com>
2023-04-10 10:59:26 +0900

    [FEAT] 카카오 로그인 버튼 에셋 추가

---
+++ src/helpers/assets.js
@@ -0,0 +1,9 @@
+import logo from "../assets/kindergrew_logo.png";
+import kakao_login_btn from "../assets/kakao_login_medium_wide.png";
+
+const ASSETS = {
+  logo: logo,
+  kakao_login_btn: kakao_login_btn,
+};
+
+export default ASSETS;
