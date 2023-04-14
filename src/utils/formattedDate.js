837595bfa669fa307589c5b13aa42bf0717719b3
Cupcakes33 <rute930627@gmail.com>
2023-04-14 22:18:27 +0900

    [FEAT]  formattedDate 유틸 함수 추가

---
+++ src/utils/formattedDate.js
@@ -0,0 +1,8 @@
+const formattedDate = (date) => {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  return `${year}-${month}-${day}`;
+};
+
+export default formattedDate
