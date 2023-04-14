d26c1bccbcffbbdc7b77b9dc0f6ea13767df57dd
Cupcakes33 <rute930627@gmail.com>
2023-04-14 10:51:04 +0900

    [FEAT] childManageAPI childSchedule 관련 api 추가

--- src/api/ChildManageAPI.js
+++ src/api/ChildManageAPI.js
@@ -4,6 +4,12 @@ const ChildManageAPI = {
   getChildProfile: (childId) => instance.get(`/parent/child/${childId}`),
   putChildProfule: ({ childId, data }) =>
     instance.put(`/parent/child/${childId}`, data),
+  getChildSchedule: (childId) =>
+    instance.get(`/parent/child/${childId}/schedule`),
+  putChildSchedule: ({ childId, data }) => {
+    console.log(childId, data);
+    instance.put(`/parent/child/${childId}/schedule`, data);
+  },
 };
 
 export default ChildManageAPI;
