437b8a19363590e1882b9d1f4d7210a5ff9b10c0
Cupcakes33 <rute930627@gmail.com>
2023-04-17 10:27:23 +0900

    [FIX] 파비콘 변경

--- src/helpers/assets.js
+++ src/helpers/assets.js
@@ -1,10 +1,12 @@
 import logo from "../assets/kindergrew_logo.png";
 import big_logo from "../assets/kindergrew_big_logo.png";
+import main_logo from "../assets/kindergrew_main_logo.png";
 import kakao_login_btn from "../assets/kakao_login_medium_wide.png";
 
 const ASSETS = {
   logo: logo,
   big_logo: big_logo,
+  main_logo: main_logo,
   kakao_login_btn: kakao_login_btn,
 };
 
